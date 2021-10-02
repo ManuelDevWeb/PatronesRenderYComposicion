8a2fc8d98f469f6b9fc872dcc2239dc81a957364
unknown <manueldeveloper17@gmail.com>
2021-10-02 02:44:19 -0500

    Se organizaron un poco la estructura de archivos y componentes

--- app_todo_list/src/App.js
+++ app_todo_list/src/App.js
@@ -1,11 +1,7 @@
-import React, { Fragment, useState } from "react";
+import React, { useState } from "react";
 
 // Importando componentes
-import { TodoCounter } from "./components/TodoCounter/TodoCounter";
-import { TodoSearch } from "./components/TodoSearch/TodoSearch";
-import { TodoList } from "./components/TodoList/TodoList";
-import { CreateTodoButton } from "./components/CreateTodoButton/CreateTodoButton";
-import { Logo } from "./components/Logo/Logo";
+import { AppUI } from "./AppUI";
 
 // Colección de TODOS
 const defaultTodos = [
@@ -43,32 +39,34 @@ function App() {
   // Controlando las tareas completadas
   const completeTodos = (text) => {
     // Obteniendo la posicion de los text que concuerden
-    const todoIndex=todos.findIndex(todo=>todo.text===text);
+    const todoIndex = todos.findIndex((todo) => todo.text === text);
     // Almacenamos todos los elementos del array todos y cambiamos el valor del atributo completed de la tarea seleccionada
-    const newTodos=[...todos];
+    const newTodos = [...todos];
     newTodos[todoIndex].completed = !newTodos[todoIndex].completed;
     setTodos(newTodos);
   };
 
   // Controlando las tareas a eliminar
-  const deleteTodos=(text)=>{
+  const deleteTodos = (text) => {
     // Obteniendo la posicion de los text que concuerden
-    const todoIndex=todos.findIndex(todo=>todo.text===text);
+    const todoIndex = todos.findIndex((todo) => todo.text === text);
     // Almacenando todos los elementos del array y eliminando la tarea seleccionada
-    const newTodos=[...todos];
+    const newTodos = [...todos];
     // Eliminando a partir de la posición encontrada, una tarea
-    newTodos.splice(todoIndex,1);
+    newTodos.splice(todoIndex, 1);
     setTodos(newTodos);
-  }
+  };
 
   return (
-    <Fragment>
-      <Logo />
-      <TodoCounter completedTodos={completedTodos} totalTodos={totalTodos} />
-      <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
-      <TodoList arrayTodos={searchedTodos} completeTodos={completeTodos} deleteTodos={deleteTodos}/>
-      <CreateTodoButton />
-    </Fragment>
+    <AppUI
+      completedTodos={completedTodos}
+      totalTodos={totalTodos}
+      searchValue={searchValue}
+      setSearchValue={setSearchValue}
+      arrayTodos={searchedTodos}
+      completeTodos={completeTodos}
+      deleteTodos={deleteTodos}
+    />
   );
 }
 
---
+++ app_todo_list/src/AppUI.js
@@ -0,0 +1,26 @@
+import React, { Fragment } from "react";
+
+// Importando componentes
+import { TodoCounter } from "./components/TodoCounter/TodoCounter";
+import { TodoSearch } from "./components/TodoSearch/TodoSearch";
+import { TodoList } from "./components/TodoList/TodoList";
+import { CreateTodoButton } from "./components/CreateTodoButton/CreateTodoButton";
+import { Logo } from "./components/Logo/Logo";
+
+const AppUI = ({completedTodos, totalTodos, searchValue, arrayTodos, setSearchValue, completeTodos, deleteTodos}) => {
+  return (
+    <Fragment>
+      <Logo />
+      <TodoCounter completedTodos={completedTodos} totalTodos={totalTodos} />
+      <TodoSearch searchValue={searchValue} setSearchValue={setSearchValue} />
+      <TodoList
+        arrayTodos={arrayTodos}
+        completeTodos={completeTodos}
+        deleteTodos={deleteTodos}
+      />
+      <CreateTodoButton />
+    </Fragment>
+  );
+};
+
+export { AppUI };
